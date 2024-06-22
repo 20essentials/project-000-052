a73cd9f0749195edcfcaf8be9c4b0c6388fcb10a
20essentials <s0l0estudi4r@gmail.com>
2024-06-22 16:59:23 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,7 @@
+console.group("REFERENCE ⬇️");
+console.log("Instagram Cat: ", "https://www.instagram.com/yuni_blackcat/");
+console.log(
+  "Scroll Driven:",
+  "https://scroll-driven-animations.style/demos/reverse-scroll/css/"
+);
+console.groupEnd();
